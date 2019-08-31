a9d12ca73df019219ccdac1b4b16a5ab8733631d
Anubhav311 <anubhavbhambri1@gmail.com>
2019-08-31 15:07:22 +0530

    added hireme.js page

---
+++ pages/hireme.js
@@ -0,0 +1,7 @@
+const HireMe = () => 
+    <div>
+        <h1>Hire Me</h1>
+        <p>You can hire me at <a href="mailto:anubhavbhambri1@gmail.comm">anubhavbhambri1@gmail.comm</a></p>
+    </div>
+
+export default HireMe

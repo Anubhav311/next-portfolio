c87bba5d7773e77b52daa240aa895cbccda22845
Anubhav311 <anubhavbhambri1@gmail.com>
2019-08-31 15:04:04 +0530

    created about.js page

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+
+export default () => 
+    <div>
+        <h1>About</h1>
+        <Link href="/">
+            <a>Go to home</a>
+        </Link>
+        <p>A JavaScript Programmer</p>
+        <img src="/static/javascript-logo.png" alt="JavaScript" height="200px"/>
+    </div>

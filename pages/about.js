c4efabc555b0d13b7a80da43f98b5528f9e4aba7
Anubhav311 <anubhavbhambri1@gmail.com>
2019-09-01 20:00:02 +0530

    created customized error page

--- pages/about.js
+++ pages/about.js
@@ -1,21 +1,26 @@
-import Link from 'next/link';
 import { Component } from 'react';
 import fetch from 'isomorphic-unfetch';
 
 import Layout from '../components/Layout';
+import Error from './_error';
 
 //ASYNC getInitialProps WITH CLASSICAL COMPONENT
 export default class About extends Component {
 
     static async getInitialProps() {
         const res = await fetch('https://api.github.com/users/Anubhav311')
+        const statusCode = res.status > 200 ? res.status : false;
         const data = await res.json();
 
-        return { user: data}
+        return { user: data, statusCode};
     }
 
     render() {
-        const { user } = this.props;
+        const { user, statusCode } = this.props;
+
+        if (statusCode) {
+            return <Error statusCode={statusCode}/>
+        }
 
         return (
             <Layout title="About">

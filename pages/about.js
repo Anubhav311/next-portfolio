5b3c9809719745a80ca577588f4772cfa140ea75
Anubhav311 <anubhavbhambri1@gmail.com>
2019-09-01 19:12:01 +0530

    fetching github data and rendering on page

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,27 @@
-import Link from 'next/link'
+import Link from 'next/link';
+import { Component } from 'react';
+import fetch from 'isomorphic-unfetch';
+
 import Layout from '../components/Layout';
 
-export default () => 
-    <Layout title="About">
-        <p>A JavaScript Programmer</p>
-        <img src="/static/javascript-logo.png" alt="JavaScript" height="200px"/>
-    </Layout>
+
+export default class About extends Component {
+
+    static async getInitialProps() {
+        const res = await fetch('https://api.github.com/users/Anubhav311')
+        const data = await res.json();
+
+        return { user: data}
+    }
+
+    render() {
+        const { user } = this.props;
+
+        return (
+            <Layout title="About">
+                <p>{user.name}</p>
+                <img src={user.avatar_url} alt="anubhav" height="200px"/>
+            </Layout>
+        )
+    }
+} 

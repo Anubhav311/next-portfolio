1ca814700fc61e4e0f839165f0ff3803fada618c
Anubhav311 <anubhavbhambri1@gmail.com>
2019-09-01 19:26:26 +0530

    getInitialProps with functional component

--- pages/about.js
+++ pages/about.js
@@ -4,7 +4,7 @@ import fetch from 'isomorphic-unfetch';
 
 import Layout from '../components/Layout';
 
-
+//ASYNC getInitialProps WITH CLASSICAL COMPONENT
 export default class About extends Component {
 
     static async getInitialProps() {
@@ -25,3 +25,26 @@ export default class About extends Component {
         )
     }
 } 
+
+
+//ASYNC getInitialProps WITH FUNCTIONAL COMPONENT
+// const About = (props) => {
+
+//     const { user } = props;
+
+//     return (
+//         <Layout title="About">
+//             <p>{user.name}</p>
+//             <img src={user.avatar_url} alt="anubhav" height="200px"/>
+//             {console.log(props)}
+//         </Layout>
+//     )
+// } 
+
+// About.getInitialProps = async ({req}) => {
+//     const res = await fetch('https://api.github.com/users/Anubhav311')
+//     const data = await res.json();
+//     return { user: data}
+// }
+
+// export default About;
